60c5310a1f9ba238b13b0b41c4ed0f4855fccb0a
sgav191 <swgavin0@gmail.com>
2021-09-24 11:32:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,23 +19,32 @@ else:
     print ("GOOD LUCK!!!!")
     
 randomnumber = random.randint(1000,9999)
-numberofmice = 0
-numberofmen = 0
 numberofattempts = 0
 gameloop = True
 
 while gameloop == True:
-    print (randomnumber)
-    userinput = int(input ("Type in what you think the 4-digit number is: "))
+
+    userinput = input ("Type in what you think the 4-digit number is: ")
+    if userinput == "STOP":
+        print (f"The random number is {randomnumber}")
+        exit()
+    userinput = int(userinput)
     if userinput == randomnumber:
         numberofattempts = numberofattempts+1
         gameloop = False
     else:
-        for number in str(userinput):
+        numberofmen = 0
+        numberofmice = 0
+        position = 0
+        for number in str(userinput ):
             if number in str(randomnumber):
-                numberofmen = numberofmen+1
-            
+                if number == str(randomnumber)[position]:
+                    numberofmice = numberofmice+1
+                else:
+                    numberofmen = numberofmen+1
+            position = position+1
+        print (f"You have {numberofmen} men and {numberofmice} mice.")
+        numberofattempts = numberofattempts+1
         
-        
-print ("Well Done! You have completed the game!")
+print ("Well Done! You got it right!")
 print (f"It took {numberofattempts} attempts to get the number, which was {randomnumber}")
